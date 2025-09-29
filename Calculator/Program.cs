0830372e84e6836d19d6c5626d1a5e6ca35b59fc
singh <singh-m21@ulster.ac.uk>
2025-09-29 16:08:07 +0100

    Added put any number in then the operation (+, -, /, *) your choice when running you can find any of the following operation out

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -2,7 +2,10 @@
 
 using System.Runtime.ExceptionServices;
 
-Console.WriteLine("Enter First Number: ");
+try
+    {
+    // First Number
+    Console.WriteLine("Enter First Number: ");
 
 // This is a string that takes input from the user
 int firstNumber = Convert.ToInt32(Console.ReadLine());
@@ -13,8 +16,47 @@ Console.WriteLine("Enter Second Number ");
 // This is a string that takes input from the user
 int secondNumber = Convert.ToInt32(Console.ReadLine());
 
-int result = firstNumber + secondNumber;
+
+Console.WriteLine("Enter the operation (+, -, * /) ");
+char operation = Convert.ToChar(Console.ReadLine());
+int result = 0;
+
+switch (operation)
+{
+    case '+':
+        result = firstNumber + secondNumber;
+        break;
+    case '-':
+        result = firstNumber - secondNumber;
+        break;
+    case '*':
+        result = firstNumber * secondNumber;
+        break;
+    case '/':
+        result = firstNumber / secondNumber;
+        break;
+    default:
+        Console.WriteLine("Invalid operation");
+        break;
+}
+
 
 // PRINTS RESULT
-Console.WriteLine(firstNumber + " + " + secondNumber + " = " + result);
+Console.WriteLine($"Result: {result}");
 
+    }
+    catch (FormatException ex)
+    {
+        // Handle the case where the input is not valid
+        Console.WriteLine("Please enter a valid number.");
+    }
+    catch(DivideByZeroException ex)
+    {
+        // Handle the divide by zero error
+        Console.WriteLine($"You cannot divide by zero.");
+    }
+    finally
+    {
+    // This block runs no matter what
+    Console.WriteLine("Operation completed.");
+}
