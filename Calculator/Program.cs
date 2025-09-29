6ff5b985d153d0370d9cefccd85e159d65e762cd
singh <singh-m21@ulster.ac.uk>
2025-09-29 16:01:26 +0100

    initial commit of software working, addition

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,1 +1,20 @@
 ﻿
+
+using System.Runtime.ExceptionServices;
+
+Console.WriteLine("Enter First Number: ");
+
+// This is a string that takes input from the user
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+// Second Number
+Console.WriteLine("Enter Second Number ");
+
+// This is a string that takes input from the user
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = firstNumber + secondNumber;
+
+// PRINTS RESULT
+Console.WriteLine(firstNumber + " + " + secondNumber + " = " + result);
+
